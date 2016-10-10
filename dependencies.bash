d2c5521808eaf8dd8c9feec5542485bc538bf962
okkuweb <okkuweb@gmail.com>
2016-10-10 09:08:30 +0300

    Some guides on enabling python support in nvim

--- dependencies.bash
+++ dependencies.bash
@@ -6,6 +6,12 @@ sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
 
-#Prerequisites for the Python modules
+# Prerequisites for the Python modules
 sudo apt-get install python-dev python-pip python3-dev python3-pip
 
+# If that isn't enough do this
+pip3 install neovim
+# Run this whenever something breaks or big updates come up
+pip3 install --upgrade neovim
+# If deoplete or something doesn't work yet do this in neovim
+:UpdateRemotePlugins
