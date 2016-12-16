7eb51c341abe1edb7ce2979daf7e6d8971d16d1d
okkuweb <okkuweb@protonmail.com>
2016-12-16 13:17:14 +0200

    Made a check if "source ~/.bash_aliases" already present in .bashrc and added some comments

--- ubuntulink.sh
+++ ubuntulink.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+# Check current folder
 location=`dirname $0`
+# Link files to appropriate locations
 ln -f $location/bash_aliases.bash ~/.bash_aliases
 ln -f $location/inputrc.bash ~/.inputrc
 ln -f $location/vimrc.vim ~/.vimrc
 ln -f $location/tmux.conf ~/.tmux.conf
 ln -f $location/tmux.conf.sh ~/.tmux.conf.sh
-echo "source ~/.bash_aliases" >> ~/.bashrc
+
+# Check if source .bash_aliases already present in .bashrc and add it there if not
+check=`grep "source ~/.bash_aliases" ~/.bashrc`
+if [ "$check" ]
+then
+    echo "Source ~/.bash_aliases already present in .bashrc"
+    exit
+else
+    echo "Added source ~/.bash_aliases to .bashrc"
+    printf "\n# Source bash aliases\nsource ~/.bash_aliases" >> ~/.bashrc
+fi
