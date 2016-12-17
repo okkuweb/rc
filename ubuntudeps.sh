4310dc9f11bc2967232a2e06d3aac2b5b0c298cc
okkuweb <okkuweb@protonmail.com>
2016-12-17 12:48:25 +0200

    Added molokai colorscheme to ubuntudeps

--- ubuntudeps.sh
+++ ubuntudeps.sh
@@ -8,3 +8,7 @@ sudo apt-get install curl git xclip vim tmux unp
 # Install pathogen package manager for vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Download molokai colorscheme for vim
+cd ~/.vim/bundle
+git clone https://github.com/tomasr/molokai
