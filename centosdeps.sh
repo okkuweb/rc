f030122b28d5f66e2c8fefb446729c18e0439526
Oskari Saari <okkuweb@gmail.com>
2017-02-11 18:02:15 +0200

    Added a bunch of tmux stuff and fixed deps

--- centosdeps.sh
+++ centosdeps.sh
@@ -9,8 +9,13 @@ sudo yum install curl git vim tmux
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# Download molokai colorscheme for vim
+# Download vim plugins
 cd ~/.vim/bundle
 git clone https://github.com/tomasr/molokai
 git clone https://github.com/airblade/vim-gitgutter
 git clone https://github.com/jiangmiao/auto-pairs
+
+# Download tmux plugins
+mkdir -p ~/.tmux && cd ~/.tmux
+git clone https://github.com/tmux-plugins/tmux-resurrect
+
