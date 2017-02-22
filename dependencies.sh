ddcfa066152a030ee1f5a65fcff38dfed317cf4f
okkuweb <okkuweb@protonmail.com>
2017-02-22 21:12:47 +0200

    Removed sensible from vimrc and combined dependency sh files

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Update repositories
-sudo yum check-update
+OS=`python -mplatform | grep -io "ubuntu\|debian\|centos"`
+os=${OS,,}
 
 # Install required bash cli tools
-sudo yum install curl git vim tmux
+if [ "$os" == "debian" ] || [ "$os" == "ubuntu" ] ; then
+    sudo apt-get update && sudo apt-get install curl git vim tmux
+elif [ "$os" == "centos" ]; then
+    sudo yum check-update && sudo yum install curl git vim tmux
+fi
 
 # Install pathogen package manager for vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
@@ -11,10 +16,12 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Download vim plugins
 cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-sensible.git
 git clone https://github.com/tomasr/molokai
 git clone https://github.com/airblade/vim-gitgutter
 git clone https://github.com/jiangmiao/auto-pairs
 git clone https://github.com/nathanaelkane/vim-indent-guides
+git clone https://github.com/tpope/vim-surround
 
 # Download tmux plugins
 mkdir -p ~/.tmux && cd ~/.tmux
