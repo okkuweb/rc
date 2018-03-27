e659b8d7bc6ee8c44ea41efd246492536f22dbe9
Oskari Saari <okkuweb@gmail.com>
2018-03-27 14:33:26 +0300

    Added some inputrc settings

--- inputrc.bash
+++ inputrc.bash
@@ -1,2 +1,11 @@
+# Perform file completion in a case insensitive fashion
 set completion-ignore-case on
 
+# Immediately add a trailing slash when autocompleting symlinks to directories
+set mark-symlinked-directories on
+
+# Display matches for ambiguous patterns at first tab press
+set show-all-if-ambiguous on
+
+# Treat hyphens and underscores as equivalent
+set completion-map-case on
