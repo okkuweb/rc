aeb8270133e5fbbd8a14925bc390917f211f5284
okkuweb <okkuweb@protonmail.com>
2018-04-04 11:06:16 +0300

    Removed completion option reveal on first tab

--- inputrc.bash
+++ inputrc.bash
@@ -4,8 +4,5 @@ set completion-ignore-case on
 # Immediately add a trailing slash when autocompleting symlinks to directories
 set mark-symlinked-directories on
 
-# Display matches for ambiguous patterns at first tab press
-set show-all-if-ambiguous on
-
 # Treat hyphens and underscores as equivalent
 set completion-map-case on
