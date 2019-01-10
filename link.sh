85d55721b1902cf9e99af7c319bcf71ffe34de06
Oskari Saari <oskari.saari@deltagon.fi>
2019-01-10 20:00:27 +0200

    Added a check for bash profile

--- link.sh
+++ link.sh
@@ -24,15 +24,27 @@ if [ "$windows" ]; then
     exit 1
 fi
 
+# Add .bash_profile for tmux
+if [ -f ~/.bash_profile ]; then
+    checkprofile=`grep bashrc ~/.bash_profile`
+    if [ -z "$checkprofile" ]; then
+        echo "source ~/.bashrc" >> ~/.bash_profile
+        echo "Added .bash_profile"
+    fi
+else
+    echo "source ~/.bashrc" > ~/.bash_profile
+    echo "Added .bash_profile"
+fi
+
+mkdir -p ~/.w3m
+mkdir -p ~/.vim/colors
 ln -fv $location/bashrc.bash ~/.bashrc
 ln -fv $location/bash_aliases.bash ~/.bash_aliases
 ln -fv $location/inputrc.bash ~/.inputrc
 ln -fv $location/vimrc.vim ~/.vimrc
 ln -fv $location/tmux.conf ~/.tmux.conf
-mkdir -p ~/.w3m
 ln -fv $location/w3mkeymap ~/.w3m/keymap
 ln -fv $location/nethackrc ~/.nethackrc
-mkdir -p ~/.vim/colors
 ln -fv $location/molokai.vim ~/.vim/colors
 
 # Add a local vimrc file
