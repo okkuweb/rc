5108abdf8fd747b049a12988ef3a659741bd2d03
Oskari Saari <oskari.saari@deltagon.fi>
2019-01-10 18:05:31 +0200

    Fixed a bunch of bash tomfoolery

--- link.sh
+++ link.sh
@@ -6,19 +6,19 @@ if [ -f $location/windows ]; then
 fi
 
 # Check if bashrc is updated
-backupcheck=`grep screen-256color`
+backupcheck=`grep screen-256color ~/.bashrc`
 # And back it up if it hasn't been updated
-if [ "$backupcheck" -eq "0" ]; then
+if [ -z "$backupcheck" ]; then
     cp ~/.bashrc ~/.bashrc.bak
     rm ~/.bash_profile
     echo "MOVE ALL YOUR LOCAL CONFIGURATIONS FROM ~/.bashrc.bak to ~/.bash_local"
 fi
 
 # Link files to appropriate locations
-if [ $windows ]; then
+if [ "$windows" ]; then
     ln -fv $location/vimrc.vim ~/_vimrc
     touch ~/.vimlocal
-    mkdir ~/vimfiles/colors
+    mkdir -p ~/vimfiles/colors
     ln -fv $location/molokai.vim ~/vimfiles/colors
     echo "Filthy windows files updated..."
     exit 1
@@ -29,24 +29,16 @@ ln -fv $location/bash_aliases.bash ~/.bash_aliases
 ln -fv $location/inputrc.bash ~/.inputrc
 ln -fv $location/vimrc.vim ~/.vimrc
 ln -fv $location/tmux.conf ~/.tmux.conf
-ln -fv $location/fbtermrc ~/.fbtermrc
-mkdir ~/.w3m
+mkdir -p ~/.w3m
 ln -fv $location/w3mkeymap ~/.w3m/keymap
 ln -fv $location/nethackrc ~/.nethackrc
-mkdir ~/.vim/colors
+mkdir -p ~/.vim/colors
 ln -fv $location/molokai.vim ~/.vim/colors
 
 # Add a local vimrc file
 touch ~/.vimlocal
 touch ~/.tmuxlocal.conf
 touch ~/.bash_local
-
-# Check if source .bash_aliases already present in .bashrc and add it there if not
-check=`grep "bash_aliases" ~/.bashrc`
-if [ "$check" ]
-then
-    echo "REMOVE 'source ~/.bash_aliases' FROM YOUR BASHRC PLEASE! (caps so you see this)"
-fi
 
 # Add git configurations to system
 if [ -f ~/.gitconfig ]; then
