46c5fdc6e44e448a4b1088978d608cafbf366490
okkuweb <okkuweb@protonmail.com>
2025-04-23 17:46:26 +0900

    Added a note to update pack.sh at some point

--- pack.sh
+++ pack.sh
@@ -2,6 +2,7 @@
 # Check current folder
 location=`dirname $0`
 
+# TODO: Have to refactor this to work with the new configuration systems
 rm "$location/rc.zip"
 zip -r "$location/rc.zip" $location
 read -p "Which server do you wish to send this to? (empty to skip)`echo $'\n> '`" fqdn
