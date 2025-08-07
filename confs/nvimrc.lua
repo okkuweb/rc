442ad08fe44a543de4f9dc1504318b5fd11c4db3
okkuweb <okkuweb@protonmail.com>
2025-08-07 15:42:08 +0900

    Added a thing to jump where the cursor was last time before closing a file in nvim

--- confs/nvimrc.lua
+++ confs/nvimrc.lua
@@ -215,4 +215,20 @@ vim.o.undodir = vim.fn.expand("~/.nvim/tempfiles")
 vim.cmd("colorscheme gruvbox")
 vim.cmd("tnoremap <Esc> <C-\\><C-n>")
 
+-- Open file where the cursor was in file before closing
+local lastplace = vim.api.nvim_create_augroup("LastPlace", {})
+vim.api.nvim_clear_autocmds({ group = lastplace })
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = lastplace,
+    pattern = { "*" },
+    desc = "remember last cursor place",
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
+
 dofile(vim.fn.expand("~/.nvimlocal.lua"))
