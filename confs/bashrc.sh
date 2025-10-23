bcf6112622f5dd1e46a8608ab307ac9d6149775e
okkuweb <okkuweb@protonmail.com>
2025-10-23 13:41:32 +0900

    Optimizing some PS1s and committing

--- confs/bashrc.sh
+++ confs/bashrc.sh
@@ -49,11 +49,17 @@ shopt -s checkwinsize
 
 ## PS1 aka bash prompt settings
 parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+    local branch
+    branch=$(git branch 2> /dev/null | sed -n '/^\*/s/^\* //p')
+    
+    # If first argument is "paren" or "()", wrap in parentheses
+    if [[ "$1" == "paren" || "$1" == "()" ]]; then
+        [[ -n "$branch" ]] && branch=" ($branch)"
+    fi
+    
+    echo "$branch"
 }
-export PS1="\[\033[38;5;208m\]\u\[$(tput sgr0)\]\[\033[38;5;202m\]@\[$(tput sgr0)\]\[\033[38;5;208m\]\h\[$(tput sgr0)\]\[\033[38;5;202m\]\$(parse_git_branch):\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\w\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\n\[\033[38;5;202m\]>\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
-## Backup PS1
-#export PS1="\[$(tput bold)\]\[\033[38;5;196m\]\u@\h\$(parse_git_branch):\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\]\w\\$ \[$(tput sgr0)\]"
+export PS1="\[\033[38;5;208m\]\u\[$(tput sgr0)\]\[\033[38;5;202m\]@\[$(tput sgr0)\]\[\033[38;5;208m\]\h\[$(tput sgr0)\]\[\033[38;5;202m\]\$(parse_git_branch 'paren'):\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\w\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\n\[\033[38;5;202m\]>\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
 
 # Extract any archive by just writing "unp"
 unp () {
