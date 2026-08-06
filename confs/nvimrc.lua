08702d14ae08daf389e304dfec79cf8aeea8c591
okkuweb <okkuweb@protonmail.com>
2026-08-06 12:21:46 +0900

    Removed diagnostics from minimap

--- confs/nvimrc.lua
+++ confs/nvimrc.lua
@@ -170,7 +170,6 @@ require("lazy").setup({
             map.setup({
                 integrations = {
                     map.gen_integration.builtin_search(),
-                    map.gen_integration.diagnostic(),
                     map.gen_integration.gitsigns(),
                 },
                 window = {
