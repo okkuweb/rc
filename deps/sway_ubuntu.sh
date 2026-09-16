b0cd4e7acdb5dd45d257adf1d41f33ef305f5879
okkuweb <okkuweb@protonmail.com>
2026-09-16 18:18:22 +0900

    Made some sway configs ubuntu compatible

--- deps/sway_ubuntu.sh
+++ deps/sway_ubuntu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install sway swayidle swaylock waybar rofi thunar dunst jq brightnessctl playerctl pavucontrol pulseaudio-utils lxqt-policykit xdg-desktop-portal-gtk xdg-desktop-portal-wlr flatpak fcitx5 fcitx5-mozc
+sudo apt install sway swayidle swaylock waybar rofi thunar dunst jq brightnessctl playerctl pavucontrol pulseaudio-utils lxqt-policykit xdg-desktop-portal-gtk xdg-desktop-portal-wlr flatpak fcitx5 fcitx5-mozc wl-clipboard
